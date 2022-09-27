8ccffed084ca2b8aa9bd7a16f4cd19e35ebd423b
Tyler-Jay-Stevahn <tyler.jay.stevahn@gmail.com>
2022-09-26 19:57:31 -0500

    added stuff

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index', index, name='index'),
+    path('home/', home, name='home'),
+    path('home/new/', second_page, name='new'),
+    path('contact/', contact, name='contact'),
+    path('date/<int:year>/', datepage, name='datepage'),
+    path('signup/', signuppage, name='signuppage'),
+    path('adduser/', adduser, name='adduser'),
     path('', landingpage, name='landingpage')
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,66 @@
+#from django.shortcuts import render, redirect
+#from .models import employees, registrationdata 
+#from .forms import registration_forms
+#from .functions import to_dataframe
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'contacts.html')
 
 def landingpage(request):
     context = {
         "greeting":"heylo"
     }
     return render(request, 'landingpage.html', context)
+
+
+def home(request):
+    context= {
+        "name":"Tyler",
+        "number":45
+    }
+    return render(request, 'home.html', context)
+
+
+def datepage(request, year):
+    article_list = employees.objects.filter(pub_date__year = year)
+    context = {
+        "year": year,
+        "article_list" : article_list
+    }
+    return render(request, 'datepage.html', context)
+
+
+def second_page(request):
+    obj = employees.objects.get(id=1)
+    context={
+        "data":obj
+    }
+    return render(request, 'second_page.html', context)
+
+
+def contact(request):
+    return render(request, 'contacts.html')
+
+
+def signuppage(request):
+
+    context = {
+        "form":registration_forms
+    }
+
+    return render(request, 'signup.html', context)
+
+
+def adduser(request):
+    form = registration_forms(request.POST)
+    if form.is_valid():
+        myregister = registrationdata(username = form.cleaned_data['username'], 
+                                    password = form.cleaned_data['password'], 
+                                    email = form.cleaned_data['email'], 
+                                    phone = form.cleaned_data['phone'])
+        myregister.save()
+        return redirect('home')
